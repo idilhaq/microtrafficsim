acfb658fe980bdc8769c6c26768254929519270d
qzed <qzed@users.noreply.github.com>
2016-06-17 19:18:01 +0200

    implement random scenario

--- microtrafficsim-ui/src/main/java/microtrafficsim/ui/tdw/simulations/RandomScenarioBuilder.java
+++ microtrafficsim-ui/src/main/java/microtrafficsim/ui/tdw/simulations/RandomScenarioBuilder.java
@@ -1,7 +1,11 @@
 package microtrafficsim.ui.tdw.simulations;
 
+import microtrafficsim.core.logic.Node;
 import microtrafficsim.core.logic.StreetGraph;
+import microtrafficsim.core.map.Coordinate;
+import microtrafficsim.core.map.area.SimplePolygon;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -9,7 +13,32 @@ public class RandomScenarioBuilder extends StartEndScenarioBuilder {
 
     @Override
     public StartEndScenarioDescription createDescription(StreetGraph graph) {
-        return new StartEndScenarioDescription(new HashMap<>(), new HashMap<>());
+        // TODO
+
+        ArrayList<Node> start = new ArrayList<>();
+        ArrayList<Node> end = new ArrayList<>();
+
+        graph.getNodeIterator().forEachRemaining(node -> {
+            double rnd = Math.random();
+            if (rnd < 0.1)
+                start.add(node);
+            else if (rnd > 0.9)
+                end.add(node);
+        });
+
+        SimplePolygon p = new SimplePolygon(new Coordinate[] {
+                new Coordinate(0.0, 0.0),
+                new Coordinate(0.0, 0.1),
+                new Coordinate(0.1, 0.1)
+        });
+
+        HashMap<SimplePolygon, ArrayList<Node>> startMap = new HashMap<>();
+        HashMap<SimplePolygon, ArrayList<Node>> endMap = new HashMap<>();
+
+        startMap.put(p, start);
+        endMap.put(p, end);
+
+        return new StartEndScenarioDescription(startMap, endMap);
     }
 
     @Override
